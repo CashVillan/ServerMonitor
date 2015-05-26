01a2ee04bf549bba331e237bbd21cb1c9c55535c
Jordan Wheatley <jordanwheatley86@yahoo.com>
2015-05-25 23:59:34 -0400

    addServers Work

--- src/me/cashvillan/servermonitor/handlers/Window.java
+++ src/me/cashvillan/servermonitor/handlers/Window.java
@@ -2,6 +2,8 @@ package me.cashvillan.servermonitor.handlers;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,6 +11,14 @@ import javax.swing.JLabel;
 import javax.swing.WindowConstants;
 
 public class Window {
+	
+	public static void getSettings(JFrame frame) {
+		frame.setSize(350, 1080);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+	}
 
 	public static void startWindow() {		
 		JFrame frame = new JFrame("Server Monitor");
@@ -19,14 +29,23 @@ public class Window {
 			label.setAlignmentY(Component.TOP_ALIGNMENT);
 			frame.add(label);
 		}
-		JButton addServersButton = new JButton("Add Servers");
-		frame.add(addServersButton, BorderLayout.SOUTH);
-		frame.pack();
-		
-		frame.setSize(350, 1080);
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		addButtons(frame);
+		getSettings(frame);
 	}
+	
+	private static void addButtons(final JFrame frame) {
+		JButton addServersButton = new JButton("Add Servers");
+		addServersButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addServersWindow(frame);
+				return;
+			}
+		});
+		frame.add(addServersButton, BorderLayout.SOUTH);
+	}
+	
+	public static void addServersWindow(JFrame frame) {
+		getSettings(frame);
+	}
+	
 }
