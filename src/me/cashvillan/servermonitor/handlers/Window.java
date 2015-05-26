6a963b59cdb86a89aa7ebd8ddada7fe1a6da0884
Jordan Wheatley <jordanwheatley86@yahoo.com>
2015-05-25 23:47:30 -0400

    Window work

--- src/me/cashvillan/servermonitor/handlers/Window.java
+++ src/me/cashvillan/servermonitor/handlers/Window.java
@@ -1,6 +1,9 @@
 package me.cashvillan.servermonitor.handlers;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Component;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.WindowConstants;
@@ -8,17 +11,22 @@ import javax.swing.WindowConstants;
 public class Window {
 
 	public static void startWindow() {		
-		JFrame frame = new JFrame("SwingSword Server Monitor");
+		JFrame frame = new JFrame("Server Monitor");
 		for (Server s : ServerManager.servers) {
 			JLabel label = new JLabel(Status.getStatus(ServerManager.getHost(s.name), ServerManager.getPort(s.name)));
-			label.setForeground(Color.WHITE);
+			label.setForeground(Status.statusColor(ServerManager.getHost(s.name), ServerManager.getPort(s.name)));
+			label.setAlignmentX(Component.CENTER_ALIGNMENT);
+			label.setAlignmentY(Component.TOP_ALIGNMENT);
 			frame.add(label);
 		}
-		frame.setSize(640, 480);
+		JButton addServersButton = new JButton("Add Servers");
+		frame.add(addServersButton, BorderLayout.SOUTH);
+		frame.pack();
+		
+		frame.setSize(350, 1080);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.getContentPane().setBackground(Color.BLACK);
 		frame.setVisible(true);
 	}
 }
